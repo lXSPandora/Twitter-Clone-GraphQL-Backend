a0ae4d9bf6765b8330545459e97d32c4af479e7d
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2017-12-16 18:37:57 -0200

    Added image Field to user

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -17,6 +17,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: {
